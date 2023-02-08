e03ffec80783ef7feee1226b54dcee05c7426dcc
Dmitriy <dmitriy.denisov@deel.com>
2023-02-08 23:51:45 +0400

    feat: BREAKING CHANGE delete all files

--- src/functions/getRegExpFlags.ts
+++
@@ -1,3 +0,0 @@
-export function getRegExpFlags(caseSensitive: boolean) {
-  return caseSensitive ? 'g' : 'gi';
-}
--- src/index.ts
+++
@@ -1,2 +0,0 @@
-export { getChunks } from './functions/getChunks';
-export { GetChunksType } from './types';
